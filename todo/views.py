c157d4a6a0eb3e3685e375e72849a859ae74fe41
ZHANGCHUANGCHUANG1 <zhangchuangchuang1@outlook.com>
2023-11-05 22:04:30 +0800

    add next video link

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-
+# https://learning.oreilly.com/videos/django-3/9781801818148/9781801818148-video7_4/
 # Create your views here.
 def signupuser(request):
     print(request.POST)
