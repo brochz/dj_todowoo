4f721d578d77cdd143e8b71cfa6174c1633480b3
ZHANGCHUANGCHUANG1 <zhangchuangchuang1@outlook.com>
2023-11-07 22:52:16 +0800

    sleep time, push the code

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-# https://learning.oreilly.com/videos/django-3/9781801818148/9781801818148-video7_4/
+# https://learning.oreilly.com/videos/django-3/9781801818148/9781801818148-video7_5/
 # Create your views here.
 def signupuser(request):
     if request.method == "GET":
         return render(request, "signupuser.html", {"form": UserCreationForm()})
     else:
-        User.objects.create_user(request.POST["username"], password=request.POST["password1"])
+        if request.POST["password1"] ==  request.POST["password2"]:
+            User.objects.create_user(request.POST["username"], password=request.POST["password1"])
+            return HttpResponse("OK OK")
+        else:
+            pass
