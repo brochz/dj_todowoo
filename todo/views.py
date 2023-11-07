d9f7e18ea9090e08e5434793412a338ab2cdb49e
Jerry Zhang <jerryzh@nvidia.com>
2023-11-07 18:55:02 +0800

    commit

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 # https://learning.oreilly.com/videos/django-3/9781801818148/9781801818148-video7_4/
 # Create your views here.
 def signupuser(request):
-    print(request.POST)
-    return render(request, "signupuser.html", {"form": UserCreationForm()})
+    if request.method == "GET":
+        return render(request, "signupuser.html", {"form": UserCreationForm()})
+    else:
+        User.objects.create_user(request.POST["username"], password=request.POST["password1"])
